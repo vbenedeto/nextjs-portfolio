105d5c1c52ac8d69cf4b1e8d7f2958167aa81757
Vittoria Benedeto <vdossantosbenedeto@tesla.com>
2024-04-28 14:14:04 +0200

    Continue to setup and create header component

---
+++ lib/data.ts
@@ -0,0 +1,50 @@
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Berlin, DE",
+    description:
+      "Graduated after 3 months of studying. 1 month after I found a job as an intern front-end developer",
+    date: "2023",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Berlin - Brandenburg, DE",
+    description:
+      "I worked as an Intern Front-End Developer for more than 1 year in Tesla Gigafactory, where I developed my skills and learned how to work with a team ",
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Typescript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+] as const;
